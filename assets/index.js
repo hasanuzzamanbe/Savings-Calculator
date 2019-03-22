e666df71a4758633808706d8ace443a1ac0d5f20
Hasanuzzaman <shamim@authlab.io>
2019-03-22 12:35:29 +0600

    new outlook

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,3 @@
-var interest = 10; // interest use 10% by hardcoded
-var tax = 5; // tax rate use 5% by hardcoded
 
 var vm = new Vue({
     el: '#app',
@@ -8,19 +6,37 @@ var vm = new Vue({
         savingsBalanceVal: '',
         savingsYearsVal: '',
         savingsRateVal: '',
+        savingsInterestVal: '',
+        savingsTaxVal: '',
         monthlySavingTotal: 0
+    },
+    methods: {
+        resetValues: function () {
+            this.savingsGoalVal= '';
+            this.savingsBalanceVal= '';
+            this.savingsYearsVal= '';
+            this.savingsRateVal= '';
+            this.savingsInterestVal='';
+            this.savingsTaxVal=''
+            document.getElementById('calculationArea').style.border = "1px solid white"
+        },
+        borderChange: function() {
+           document.getElementById('calculationArea').style.border = "1px solid #d2473d";;
+        }
     },
     computed: {
         cal_sav_goal_month: function () {
             var goal = this.savingsGoalVal;
             var years = this.savingsYearsVal;
+            var interest = this.savingsInterestVal;
+            var tax = this.savingsTaxVal;
             var cur_balance = this.savingsBalanceVal;
             var interest_rate = (interest / 100) * ((100 - tax) / 100);
             this.monthlySavingTotal = (goal - cur_balance) / ((Math.pow(1 + (interest_rate / 12), years * 12) - 1) / (interest_rate / 12));
             return parseFloat(Math.round(this.monthlySavingTotal * 100) / 100).toFixed(2);
         },
         cal_sav_goal_week: function () {
-            return parseFloat(Math.round((this.monthlySavingTotal / 7) * 100) / 100).toFixed(2);
+            return parseFloat(Math.round((this.monthlySavingTotal / 4) * 100) / 100).toFixed(2);
         },
         numberCheck: function () {
             return this.cal_sav_goal_month == Infinity || this.cal_sav_goal_month == 'NaN'
